170129630625605e14e50ece94c3c08f30cdbed2
qq076qq076 <qq076qq076@gmail.com>
2019-10-15 14:09:20 +0800

    移除沒用到的元件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { ShowContentComponent } from './show-content/show-content.component';
-import { PopupWindowComponent } from './popup-window/popup-window.component';
 import { OverlayComponent } from './popup-window/overlay/overlay.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShowContentComponent,
-    PopupWindowComponent,
     OverlayComponent
   ],
   imports: [
